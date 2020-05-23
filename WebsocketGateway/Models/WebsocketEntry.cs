4ffa5acdcb69a64ba99278866a99d005cbf9162f
Niklas <36010519+neon-js@users.noreply.github.com>
2020-05-23 12:25:09 +0200

    feature: Simple frontend
    - Added a simple Frontend which shows the data coming from the backend via SignalR

--- WebsocketGateway/Models/WebsocketEntry.cs
+++ WebsocketGateway/Models/WebsocketEntry.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics.CodeAnalysis;
 using OgnGateway.ogn.models;
 
@@ -11,9 +12,39 @@ namespace WebsocketGateway.Models
     public class WebsocketEntry
     {
         /// <summary>
-        /// Flight data as obtained by converter
+        /// Current speed of the aircraft in km/h
         /// </summary>
-        public FlightData FlightData { get; }
+        public float Speed { get; } 
+        
+        /// <summary>
+        /// Current altitude of the aircraft in m
+        /// </summary>
+        public float Altitude { get; }
+        
+        /// <summary>
+        /// Current vertical speed of the aircraft in m/s
+        /// </summary>
+        public float VerticalSpeed { get; }
+        
+        /// <summary>
+        /// Current turn rate of the aircraft in turns/min
+        /// </summary>
+        public float TurnRate { get; }
+        
+        /// <summary>
+        /// Current course of the aircraft
+        /// </summary>
+        public float Course { get; }
+        
+        /// <summary>
+        /// Current position of the aircraft
+        /// </summary>
+        public Position Position { get; }
+        
+        /// <summary>
+        /// DateTime of when the message was received
+        /// </summary>
+        public DateTime DateTime { get; }
         
         /// <summary>
         /// Aircraft data
@@ -22,7 +53,13 @@ namespace WebsocketGateway.Models
 
         public WebsocketEntry(FlightData flightData, Aircraft aircraft)
         {
-            FlightData = flightData;
+            Speed = flightData.Speed;
+            Altitude = flightData.Altitude;
+            VerticalSpeed = flightData.VerticalSpeed;
+            TurnRate = flightData.TurnRate;
+            Course = flightData.Course;
+            Position = flightData.Position;
+            DateTime = flightData.DateTime;
             Aircraft = aircraft;
         }
     }
