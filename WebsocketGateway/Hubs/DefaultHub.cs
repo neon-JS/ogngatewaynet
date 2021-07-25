67eee49fe0208b597fce807c095efcd97f1bbdf6
Niklas <36010519+neon-JS@users.noreply.github.com>
2021-07-25 16:13:32 +0200

    feat: Added endpoints for inital data & config
    - This should be used instead of SignalR

--- WebsocketGateway/Hubs/DefaultHub.cs
+++ WebsocketGateway/Hubs/DefaultHub.cs
@@ -1,8 +1,4 @@
-using System.Collections.Generic;
 using Microsoft.AspNetCore.SignalR;
-using OgnGateway.Dtos;
-using WebsocketGateway.Dtos;
-using WebsocketGateway.Providers;
 
 namespace WebsocketGateway.Hubs
 {
@@ -12,58 +8,5 @@ namespace WebsocketGateway.Hubs
     // ReSharper disable once ClassNeverInstantiated.Global
     public class DefaultHub : Hub
     {
-        /// <summary>
-        /// Data-provider which gives us the currently active aircraft.
-        /// </summary>
-        private readonly LatestDataProvider _latestDataProvider;
-
-        /// <summary>
-        /// Our current configuration
-        /// </summary>
-        private readonly GatewayConfiguration _gatewayConfiguration;
-
-        /// <summary>
-        /// APRS-config containing the filter radius & position
-        /// </summary>
-        private readonly AprsConfig _aprsConfig;
-
-        public DefaultHub(
-            LatestDataProvider latestDataProvider,
-            GatewayConfiguration gatewayConfiguration,
-            AprsConfig aprsConfig
-            )
-        {
-            _latestDataProvider = latestDataProvider;
-            _gatewayConfiguration = gatewayConfiguration;
-            _aprsConfig = aprsConfig;
-        }
-
-        /// <summary>
-        /// Method that is "called" by SignalR-clients (via some SignalR-magic).
-        /// Returns the currently active aircraft-information to the requesting client.
-        /// </summary>
-        /// <returns></returns>
-        // ReSharper disable once UnusedMember.Global
-        public IReadOnlyList<FlightDataDto> GetCurrentlyActiveFlightData()
-        {
-            return _latestDataProvider.GetLatestData();
-        }
-
-        /// <summary>
-        /// Returns the current configuration of the system
-        /// </summary>
-        /// <returns>object</returns>
-        // ReSharper disable once UnusedMember.Global
-        public object GetConfiguration()
-        {
-            return new
-            {
-                _gatewayConfiguration.MaxAgeSeconds,
-                _gatewayConfiguration.EventsOnly,
-                _gatewayConfiguration.IntervalSeconds,
-                _aprsConfig.FilterPosition,
-                _aprsConfig.FilterRadius
-            };
-        }
     }
 }
--- WebsocketGateway/Providers/LatestDataProvider.cs
+++ WebsocketGateway/Providers/LatestDataProvider.cs
@@ -5,14 +5,13 @@ using WebsocketGateway.Actors;
 using WebsocketGateway.Controllers;
 using WebsocketGateway.Dtos;
 using WebsocketGateway.Extensions.DateTime;
-using WebsocketGateway.Hubs;
 
 namespace WebsocketGateway.Providers
 {
     /// <summary>
     /// A provider that is used to hold active airplane-data.
     /// The messages are provided by the <see cref="PublishActor"/> and have to be
-    /// stored in here to pass them to the <see cref="DefaultHub"/> or <see cref="CurrentlyActiveController"/>.
+    /// stored in here to pass them to the  <see cref="ApiController"/>.
     /// </summary>
     public class LatestDataProvider
     {
