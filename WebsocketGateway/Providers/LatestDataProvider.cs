5b8ab55d4ead470b4e6c757c3eb493519223b78a
Niklas <36010519+neon-JS@users.noreply.github.com>
2021-07-25 16:13:31 +0200

    chore: Cleaned up providers

--- WebsocketGateway/Providers/LatestDataProvider.cs
+++ WebsocketGateway/Providers/LatestDataProvider.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Akka.Util.Internal;
 using WebsocketGateway.Actors;
+using WebsocketGateway.Controllers;
 using WebsocketGateway.Dtos;
 using WebsocketGateway.Extensions.DateTime;
 using WebsocketGateway.Hubs;
@@ -9,9 +10,9 @@ using WebsocketGateway.Hubs;
 namespace WebsocketGateway.Providers
 {
     /// <summary>
-    /// A provider that is used in the <see cref="DefaultHub"/> to give them the currently active
-    /// airplane-data. The messages are provided by the <see cref="PublishActor"/> and have to be
-    /// stored in here to pass them to the <see cref="DefaultHub"/>.
+    /// A provider that is used to hold active airplane-data.
+    /// The messages are provided by the <see cref="PublishActor"/> and have to be
+    /// stored in here to pass them to the <see cref="DefaultHub"/> or <see cref="CurrentlyActiveController"/>.
     /// </summary>
     public class LatestDataProvider
     {
@@ -46,21 +47,13 @@ namespace WebsocketGateway.Providers
         /// <returns>currently active flight-data</returns>
         public IReadOnlyList<FlightDataDto> GetLatestData()
         {
-            CleanUpEntries();
-            return _activeFlightData.Values.ToList();
-        }
-
-        /// <summary>
-        /// Removes outdated entries from the <see cref="_activeFlightData"/>-list.
-        /// We call this method on demand as an interval-call would not make sense.
-        /// </summary>
-        private void CleanUpEntries()
-        {
+            /* Removes outdated entries. We call this method on demand as an interval-call would not make sense.*/
             _activeFlightData
-                .ToList()
                 .Select(entry => entry.Value)
                 .Where(dto => dto.DateTime.AddSeconds(_gatewayConfiguration.MaxAgeSeconds).IsInPast())
                 .ForEach(dto => _activeFlightData.Remove(dto.Aircraft.Id));
+
+            return _activeFlightData.Values.ToList();
         }
     }
 }
